cad2186cc7f3fee9e533d8dfea689df884bc74a2
David Grammatico <david.lugdunum@gmail.com>
2023-10-01 21:43:49 +0200

    first commit, install uploadthing

---
+++ app/upload-button/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UploadButtonPage() {
+  return (
+    <div>UploadButton</div>
+  )
+}
---
+++ app/upload-dnd/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UploadDnDPage() {
+  return (
+    <div>page</div>
+  )
+}
